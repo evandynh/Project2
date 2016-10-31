5e41b713e7ab087d7d3c88bf6ddc090de0f0016c
viviana rishe <vivianacuri@gmail.com>
2016-10-31 13:38:42 -0700

    adds skills routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
+
+  get 'users/:user_id/skills/new' => 'skills#new', as: :new_skill
+  get 'users/:user_id/skills/:id' => 'skills#show', as: :skill
+  get 'users/:user_id/skills/:id/edit' => 'skills#edit', as: :edit_skill
+  
 
   get 'users/' => 'users#index'
   post 'users/' => 'users#create'
@@ -14,4 +18,5 @@ Rails.application.routes.draw do
   post 'login/' => 'sessions#create'
   delete 'logout/' => 'sessions#destroy'
 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
