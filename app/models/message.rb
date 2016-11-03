7b8419eab26600a0302f734a2c1476e2eabfc2af
viviana rishe <vivianacuri@gmail.com>
2016-11-03 11:53:18 -0700

    testing

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :skill, optional: true
-  belongs_to :to, class_name: 'User'
-  belongs_to :from, class_name: 'User'
+  belongs_to :to, class_name: 'User', dependent: :destroy
+  belongs_to :from, class_name: 'User', dependent: :destroy
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,4 @@
 class Skill < ApplicationRecord
-  belongs_to :category
+  belongs_to :category, dependent: :destroy
   belongs_to :user, :optional => true
 end
