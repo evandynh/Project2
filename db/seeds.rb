f42c0e9a817b968f2fcc52f8c9d9a5d6a02bd4c2
viviana rishe <vivianacuri@gmail.com>
2016-11-03 09:52:53 -0700

    commented seeds, they are already migrated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-categories = Category.create([
-  {name: "Professional Service"}, {name: "Hobby"}
-  ])
+# categories = Category.create([
+#   {name: "Professional Service"}, {name: "Hobby"}
+#   ])
